103b2a8335c5ddf9bf5f165b8555cd4cade8b14f
Vasi Venkatesh <167093427+venkey12319@users.noreply.github.com>
2024-06-02 13:01:04 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
 # tests/test_main.py
-from src.main import add
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from main import add
 
 def test_add():
     assert add(1, 2) == 3
